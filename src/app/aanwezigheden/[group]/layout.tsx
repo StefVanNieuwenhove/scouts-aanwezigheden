78bc0b68185bda06bbabbdc10e0d595513c4d196
Stef Van Nieuwenhove <stef.vannieuwenhove@student.hogent.be>
2024-09-13 12:51:49 +0200

    remove navlink and replaced it with button link variant

--- src/app/aanwezigheden/[group]/layout.tsx
+++ src/app/aanwezigheden/[group]/layout.tsx
@@ -1,6 +1,8 @@
 import { NavLink } from '@/components/navigation';
+import { Button } from '@/components/ui/button';
 import { Separator } from '@/components/ui/separator';
 import { Group } from '@prisma/client';
+import Link from 'next/link';
 
 type AanwezigheidLayoutProps = {
   children: React.ReactNode;
@@ -12,15 +14,19 @@ const AanwezighedenLayout = ({ children, params }: AanwezigheidLayoutProps) => {
     <>
       <main className='container mx-auto w-full overflow-x-scroll mt-1'>
         <nav className='w-full flex flex-col md:flex-row justify-center gap-1 md:gap-4 items-center border-gray-200 py-4'>
-          <NavLink name='Overzicht' href={`/aanwezigheden/${params.group}`} />
-          <NavLink
-            name='Nieuwe vergadering'
-            href={`/aanwezigheden/${params.group}/create`}
-          />
-          <NavLink
-            name='Bewerk vergaderingen'
-            href={`/aanwezigheden/${params.group}/update`}
-          />
+          <Button variant={'link'}>
+            <Link href={`/aanwezigheden/${params.group}`}>Overzicht</Link>
+          </Button>
+          <Button variant={'link'}>
+            <Link href={`/aanwezigheden/${params.group}/create`}>
+              Nieuwe vergadering
+            </Link>
+          </Button>
+          <Button variant={'link'}>
+            <Link href={`/aanwezigheden/${params.group}/update`}>
+              Bewerk vergaderingen
+            </Link>
+          </Button>
         </nav>
         <Separator className='mb-5' />
         {children}
--- src/app/leden/layout.tsx
+++ src/app/leden/layout.tsx
@@ -1,5 +1,7 @@
 import { NavLink } from '@/components/navigation';
+import { Button } from '@/components/ui/button';
 import { Separator } from '@/components/ui/separator';
+import Link from 'next/link';
 
 type LedenLayoutProps = {
   children: React.ReactNode;
@@ -9,9 +11,15 @@ const LedenLayout = async ({ children }: LedenLayoutProps) => {
   return (
     <main className='container mx-auto my-1 w-full overflow-x-scroll'>
       <nav className='w-full flex justify-center gap-1 md:gap-4 items-center border-gray-200 py-4'>
-        <NavLink name='Overzicht' href='/leden' />
-        <NavLink name='CSV file' href='/leden/csv' />
-        <NavLink name='Form' href='/leden/form' />
+        <Button variant={'link'}>
+          <Link href='/leden'>Overzicht</Link>
+        </Button>
+        <Button variant={'link'}>
+          <Link href='/leden/csv'>CSV file</Link>
+        </Button>
+        <Button variant={'link'}>
+          <Link href='/leden/form'>Form</Link>
+        </Button>
       </nav>
       <Separator className='mb-5' />
       <section>{children}</section>
